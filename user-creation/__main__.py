17f10f3bbb3d25113aa4aad8cb6cc45f214a7529
Destiny <68116419+heyitisdestiny@users.noreply.github.com>
2022-08-15 09:24:37 +0100

    Setup Start

--- user-creation/__main__.py
+++ user-creation/__main__.py
@@ -1,3 +1,11 @@
+"""
+RadioPanel Creations: User Creation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the Discord API.
+Copyright: (c) 2022 Destiny Lumley
+Llicense: MIT
+"""
+
 from typing import Union
 from json import loads
 
@@ -13,10 +21,22 @@ def open_config() -> Union[None, dict]:
 
 
 def setup() -> None:
-    pass
+    print_setup()
+    api_key = input("API Key: ")
+    
+
+def print_setup() -> None:
+    print("This tool has not been configured.")
+    print("Please follow the prompts bellow to setup this tool.")
+    print("")
+
+def print_welcome() -> None:
+    print("RadioPanel Creations: User Creation")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 def main() -> None:
+    print_welcome()
     config = open_config()
     if config is None:
         return setup()
